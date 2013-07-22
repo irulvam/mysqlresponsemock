b03868b441ff18246a3f40b370d4a6fc072dfc83
Rafal Zajac <rzajac@gmail.com>
2013-07-22 21:00:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## REALLY simple tool to mock MySQL responses
+
+I needed a simple response mock for the *github.com/ziutek/mymysql/mysql* responses. Using [gomock](http://godoc.org/code.google.com/p/gomock/gomock) seamed like overkill.
+
+## Example usage
+
+```go
+
+import (
+	"github.com/rzajac/assert/assert"
+)
+
+type testStruct struct {
+	Id    string
+	Field string
+}
+
+func Test_initFromDb(t *testing.T) {
+	row, res := mysqlresultmock.NewResponseMock("Id", 4, "Field", 5)
+
+	tStruct := new(testStruct)
+	err := tStruct.initFromDB(row, res)
+	assert.NotError(t, err)
+
+	assert.Equal(t, "4", tStruct.Id)
+	assert.Equal(t, "5", tStruct.Field)
+}
+```
+
+## License
+
+Released under the MIT License.
+Mysqlresponsemock (c) Rafal Zajac <rzajac@gmail.com>
---
+++ mysqlresponsemock.go
@@ -0,0 +1,124 @@
+package mysqlresponsemock
+
+import (
+	"github.com/ziutek/mymysql/mysql"
+)
+
+// The structure is coppied from the original code
+type ResultMock struct {
+	Field_count int
+	Fieldsf     []*mysql.Field // Fields table
+	Fc_map      map[string]int // Maps field name to column number
+}
+
+// NewResponseMock mocks MySQL response.
+//
+// Example:
+//
+//     row, resp := NewResponseMock("Id", 1, "Name": "Tom")
+//
+func NewResponseMock(v ...interface{}) (mysql.Row, mysql.Result) {
+
+	if len(v)%2 != 0 {
+		panic("Number of arguments must be even")
+	}
+
+	var colValues []interface{}
+	var colName string
+	var ok bool
+	colNames := []string{}
+
+	for i := 0; i < len(v); i += 2 {
+		colName, ok = v[i].(string)
+		if !ok {
+			panic("Columns must be strings")
+		}
+		colNames = append(colNames, colName)
+		colValues = append(colValues, v[i+1])
+	}
+
+	return colValues, NewResultMock(colNames)
+}
+
+// NewResultMock mocks mysql.Result
+// NOTE: Right now it only sets mapping between column indexes and column names.
+func NewResultMock(columns []string) *ResultMock {
+	rm := new(ResultMock)
+	rm.Field_count = len(columns)
+	rm.Fc_map = make(map[string]int)
+
+	for idx, colName := range columns {
+		rm.Fc_map[colName] = idx
+	}
+	return rm
+}
+
+// Returns index for given name or -1 if field of that name doesn't exist
+func (res *ResultMock) Map(field_name string) int {
+	if fi, ok := res.Fc_map[field_name]; ok {
+		return fi
+	}
+	return -1
+}
+
+// Returns true if this is status result that includes no result set
+func (res *ResultMock) StatusOnly() bool {
+	panic("Not implemented.")
+}
+
+// Returns a table containing descriptions of the columns
+func (res *ResultMock) Fields() []*mysql.Field {
+	panic("Not implemented.")
+}
+
+func (res *ResultMock) Message() string {
+	panic("Not implemented.")
+}
+
+func (res *ResultMock) AffectedRows() uint64 {
+	panic("Not implemented.")
+}
+
+func (res *ResultMock) InsertId() uint64 {
+	panic("Not implemented.")
+}
+
+func (res *ResultMock) WarnCount() int {
+	panic("Not implemented.")
+}
+
+func (res *ResultMock) MakeRow() mysql.Row {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) End() error {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) GetFirstRow() (mysql.Row, error) {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) GetLastRow() (mysql.Row, error) {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) GetRow() (mysql.Row, error) {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) GetRows() ([]mysql.Row, error) {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) MoreResults() bool {
+	return false
+}
+
+func (r *ResultMock) NextResult() (mysql.Result, error) {
+	panic("Not implemented.")
+}
+
+func (r *ResultMock) ScanRow(row mysql.Row) error {
+	panic("Not implemented.")
+}
